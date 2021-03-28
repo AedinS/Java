20f0b581a115258321ddaf7dfc6552f9c303a518
AedinS <79467832+AedinS@users.noreply.github.com>
2021-03-28 21:45:29 +0900

    Pong game is upgraded!
    added score, gameOver board🐝

--- PingPong_Upgrade.java
+++ PingPong_Upgrade.java
@@ -8,15 +8,21 @@ public class PingPong extends JPanel implements KeyListener {
 	private Ball ball;
 	private Racquet racquet1;
 	private Racquet racquet2;
-
+	private Score score;
+	private int scoreL, scoreR;
+	private static boolean start=true;
 	
 	public PingPong() {
-		ball = new Ball(this, Color.red); // this - 라켓과 상호작용 하기 위해
-		this.setBackground(Color.green);
-		racquet1 = new Racquet(this, 10, 150, Color.blue); // this - same
-		racquet2 = new Racquet(this, 560, 150, Color.yellow);
+
+		
+		ball = new Ball(this, Color.white); // this - 라켓과 상호작용 하기 위해
+		this.setBackground(Color.black);
+		racquet1 = new Racquet(this, 10, 150, 1); // this - same
+		racquet2 = new Racquet(this, 580, 150, 2);
+		score = new Score();
 		this.setFocusable(true);
 		this.addKeyListener(this);
+
 	}
 	
 	
@@ -31,6 +37,13 @@ public class PingPong extends JPanel implements KeyListener {
 		// TODO Auto-generated method stub
 		racquet1.keyPressed(e);
 		racquet2.keyPressed(e);
+		
+		if(start==false) {
+			if(e.getKeyCode()==KeyEvent.VK_ESCAPE) {
+				System.exit(0);
+			}
+		}
+		
 	}
 
 	@Override
@@ -45,7 +58,6 @@ public class PingPong extends JPanel implements KeyListener {
 		racquet1.move();
 		racquet2.move();
 	}
-	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -53,8 +65,10 @@ public class PingPong extends JPanel implements KeyListener {
 		ball.draw(g2d);
 		racquet1.draw(g2d);
 		racquet2.draw(g2d);
-
+		score.draw(g2d);
 	}
+	
+
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("PingPong Game");
@@ -63,8 +77,10 @@ public class PingPong extends JPanel implements KeyListener {
 		
 		PingPong game = new PingPong();
 		frame.add(game);
+		frame.setResizable(false);
 		frame.setVisible(true);
-		while (true) {
+
+		while (start==true) {
 			game.move();
 			game.repaint();
 			try {
@@ -73,41 +89,76 @@ public class PingPong extends JPanel implements KeyListener {
 				e.printStackTrace();
 			}
 		}
+		
+
 	}
 	
 	
 	
 	class Ball {
-		private static final int RADIUS = 20;
-		private int x = 5, y = 0, xSpeed = 1, ySpeed = 1;
+		private static final int RADIUS = 10;
+		private int x = 50, y = 50, xSpeed = 2, ySpeed = 2;
 		private PingPong game;
 		private Color color;
-
 		
 		public Ball(PingPong game, Color color) {
 			this.game = game;
 			this.color = color;
 		}
 		
-		void move() {
-			if (x+xSpeed < 0) {
-				xSpeed = 1;
+		void reborn() {
+			try {
+				Thread.sleep(3000);
+			} catch(InterruptedException e) {
+				
 			}
-			if (x+xSpeed>game.getWidth()-2*RADIUS) {
-				xSpeed = -1;
+			ball.x=300;
+			ball.y=200;
+			racquet1.x=10;
+			racquet1.y=150;
+			racquet2.x=560;
+			racquet2.y=150;
+		}
+		
+		void move() {
+			
+			if (x+xSpeed < 0) { //왼쪽 
+				xSpeed=2;
+				++scoreR;
+				reborn();
+			}
+			
+
+			if (x+xSpeed>game.getWidth()-2*RADIUS) { //오른쪽
+				xSpeed=-2;
+				++scoreL;
+				reborn();
 			}
 
-			if (y+ySpeed<0)
-				ySpeed = 1;
+
+			if (y+ySpeed<0) //위
+				ySpeed = 2;
+			
 			if(y+ySpeed>game.getHeight()-2*RADIUS)
-				ySpeed=-1;
-			if (collision())
-				xSpeed = -xSpeed;
+				ySpeed=-2; //아래 -1
+			
+			if (collision()) {
+				xSpeed = -xSpeed;	
+			}
+			
+			if (scoreL==10||scoreR==10) {
+				gameOver();
+			}
+
 			x += xSpeed;
 			y += ySpeed;
 		}
 		
-
+		public void gameOver() {
+			if(scoreL==10||scoreR==10) {
+				start=false;
+			}
+		}
 		
 		private boolean collision() {
 			return game.racquet1.getBounds().intersects(getBounds())
@@ -118,30 +169,29 @@ public class PingPong extends JPanel implements KeyListener {
 		public void draw(Graphics2D g) {
 			g.setColor(color);
 			g.fillOval(x, y, 2*RADIUS, 2*RADIUS);
-			//g.drawString("노랑" + score_1, 100, 100);
-			//g.drawString("파랑" + score_2, 50, 50);
 		}
 		
 		public Rectangle getBounds() {
 			return new Rectangle(x,y,2*RADIUS,2*RADIUS);
 		}
-		
+	
 	}
+	
+	
 	
 	class Racquet {
 		private static final int WIDTH = 10;
 		private static final int HEIGHT = 80;
 		private int x = 0, y = 0;
-		private int xSpeed = 0;
 		private int ySpeed = 0;
 		private PingPong game;
-		private Color color;
+		private int id;
 		
-		public Racquet (PingPong game, int x, int y, Color color) {
+		public Racquet (PingPong game, int x, int y, int id) {
 			this.game=game;
 			this.x=x;
 			this.y=y;
-			this.color=color;
+			this.id=id;
 		}
 		
 		public void move() {
@@ -150,7 +200,12 @@ public class PingPong extends JPanel implements KeyListener {
 		}
 		
 		public void draw(Graphics2D g) {
-			g.setColor(color);
+			if(id==1) {
+				g.setColor(Color.blue);
+			}
+			if(id==2) {
+				g.setColor(Color.red);
+			}
 			g.fillRect(x, y, WIDTH, HEIGHT);
 
 		}
@@ -160,15 +215,18 @@ public class PingPong extends JPanel implements KeyListener {
 		}
 		
 		public void keyPressed(KeyEvent e) {
-
+			if(id==1) {
 				if (e.getKeyCode()==KeyEvent.VK_W)
 					racquet1.ySpeed=-3;
 				else if (e.getKeyCode()==KeyEvent.VK_S)
 					racquet1.ySpeed=3;
-				else if (e.getKeyCode()==KeyEvent.VK_UP)
+			}
+			if(id==2) {
+				if (e.getKeyCode()==KeyEvent.VK_UP)
 					racquet2.ySpeed=-3;
 				else if (e.getKeyCode()==KeyEvent.VK_DOWN)
 					racquet2.ySpeed=3;
+			}
 		}
 		
 		public Rectangle getBounds() {
@@ -179,5 +237,40 @@ public class PingPong extends JPanel implements KeyListener {
 	}
 	
 	
-}
+	
+	public class Score {
 
+		private int game_w=600;
+		private int game_h=400;
+		private String str="";
+		Font font = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+		
+
+		public void draw(Graphics g) {
+			g.setColor(Color.white);
+			g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 60));
+
+			g.drawLine(game_w/2, 0, game_w/2, game_h);
+
+			g.drawString(String.valueOf(scoreL/10)+String.valueOf(scoreL%10), game_w/2-85, 50);
+			g.drawString(String.valueOf(scoreR/10)+String.valueOf(scoreR%10), game_w/2+20, 50);
+			
+			if(start==false) {
+				if(start==false) {
+					g.setColor(Color.BLACK);
+					g.fillRect(0, 0, 600, 600);
+
+					g.setColor(Color.RED);
+					str=(scoreL==10) ? "Player1 wins" : "Player2 wins";
+					g.drawString(str, 100, 170);
+					
+					g.setColor(Color.white);
+					g.setFont(font);
+					g.drawString("PRESS ESC TO QUIT", 200, 300);
+				}
+			}
+		}
+	}
+	
+	
+}
