3158f43c3cb608267068a421b6dcb961eca7cafd
AedinS <79467832+AedinS@users.noreply.github.com>
2021-06-04 19:46:28 +0900

    Add files via upload

---
+++ PostJson.java
@@ -0,0 +1,27 @@
+package afterMid;
+
+import java.net.*;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+
+public class PostJson {
+
+	
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		Gson gson = new Gson();
+		
+		String site = "";
+		
+		// 연결 
+		// 결과를 입력스트립으로 받아서 문자열로 저장.
+		// 저장된 문자로부터 자바 객체 생성
+		
+		// 자바 객체를 이용해서 여러가지 처리를 할 수 있겠지..
+		// Example, DB에 저장할 수도 있고....
+		// 
+	}
+
+}
